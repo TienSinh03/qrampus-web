fd4220b2d1c8316b24b1980448bf6eedc336ac4a
TienSinh03 <zatos1232@gmail.com>
2025-11-25 09:25:17 +0700

    revert commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
-// src/App.jsx
-import AppRoutes from "./routes";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
   return (
